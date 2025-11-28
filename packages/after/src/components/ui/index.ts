68cc49057a75de1686ff221d816a00d9d729cc83
parksubeom <sooknise@naver.com>
2025-11-28 13:39:25 +0900

    chore : index경로에 Native Select 추가

--- packages/after/src/components/ui/index.ts
+++ packages/after/src/components/ui/index.ts
@@ -10,3 +10,4 @@ export { FormSelect } from "./FormSelect";
 export { FormTextarea } from "./FormTextarea";
 export { FormCheckbox } from "./FormCheckbox";
 export { Label } from "./label";
+export { NativeSelect } from "./NativeSelect";
